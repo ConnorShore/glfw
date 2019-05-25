68f0c239d46cefe5fa2509a3a5c26b2498621649
Connor Shore <cjshore12@gmail.com>
2019-05-24 22:42:48 -0600

    Updated premake file

--- premake5.lua
+++ premake5.lua
@@ -7,7 +7,13 @@ project "glfw"
 
     files
     {
-        
+        "include/glfw3.h",
+        "include/glfw3native.h",
+        "src/glfw_config.h",
+        "src/context.c",
+        "src/init.c",
+        "src/window.c",
+        "src/monitor.c"   
     }
 
     filter "system:windows"
@@ -16,13 +22,20 @@ project "glfw"
 
         files
         {
-            "include",
-            "src/*.h",
-            "src/*.c"
+            "src/win32_init.c",
+            "src/win32_window.c",
+            "src/win32_time.c",
+            "src/win32_joystick.c",
+            "src/win32_monitor.c",
+            "src/win32_thread.c",
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c"
         }
 
         defines
         {
+            "_GLFW_WIN32",
             "_CRT_SECURE_NO_WARNINGS"
         }
 
